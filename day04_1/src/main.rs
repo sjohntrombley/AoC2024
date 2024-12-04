99049c2f3e9baa13cfdf15a2f6cac6655c7ff7fd
S. John Trombley <s.john.trombley@gmail.com>
2024-12-03 23:16:36 -0700

    reformat

--- day04_1/src/main.rs
+++ day04_1/src/main.rs
@@ -3,7 +3,11 @@ use std::fs::read_to_string;
 const MAS: [char; 3] = ['M', 'A', 'S'];
 
 fn main() {
-    let input: Vec<Vec<_>> = read_to_string("input.txt").unwrap().lines().map(|l| l.chars().collect()).collect();
+    let input: Vec<Vec<_>> = read_to_string("input.txt")
+        .unwrap()
+        .lines()
+        .map(|l| l.chars().collect())
+        .collect();
     let height = input.len();
     let width = input.first().unwrap().len();
     let mut ans = 0;
@@ -13,28 +17,29 @@ fn main() {
                 continue;
             }
 
-            if j > 2 && input[i][j-3..j] == ['S', 'A', 'M'] {
+            if j > 2 && input[i][j - 3..j] == ['S', 'A', 'M'] {
                 ans += 1;
             }
-            if j < width-3 && input[i][j+1..j+4] == MAS {
+            if j < width - 3 && input[i][j + 1..j + 4] == MAS {
                 ans += 1;
             }
-            if i > 2 && (1..4).all(|k| input[i-k][j] == MAS[k-1]) {
+            if i > 2 && (1..4).all(|k| input[i - k][j] == MAS[k - 1]) {
                 ans += 1;
             }
-            if i < width-3 && (1..4).all(|k| input[i+k][j] == MAS[k-1]) {
+            if i < width - 3 && (1..4).all(|k| input[i + k][j] == MAS[k - 1]) {
                 ans += 1;
             }
-            if i > 2 && j > 2 && (1..4).all(|k| input[i-k][j-k] == MAS[k-1]) {
+            if i > 2 && j > 2 && (1..4).all(|k| input[i - k][j - k] == MAS[k - 1]) {
                 ans += 1;
             }
-            if i < height-3 && j > 2 && (1..4).all(|k| input[i+k][j-k] == MAS[k-1]) {
+            if i < height - 3 && j > 2 && (1..4).all(|k| input[i + k][j - k] == MAS[k - 1]) {
                 ans += 1;
             }
-            if i > 2 && j < width-3 && (1..4).all(|k| input[i-k][j+k] == MAS[k-1]) {
+            if i > 2 && j < width - 3 && (1..4).all(|k| input[i - k][j + k] == MAS[k - 1]) {
                 ans += 1;
             }
-            if i < height-3 && j < width-3 && (1..4).all(|k| input[i+k][j+k] == MAS[k-1]) {
+            if i < height - 3 && j < width - 3 && (1..4).all(|k| input[i + k][j + k] == MAS[k - 1])
+            {
                 ans += 1;
             }
         }
