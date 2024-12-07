024296436c62460d01dd40541b392b3b74037bb9
S. John Trombley <s.john.trombley@gmail.com>
2024-12-06 23:37:39 -0700

    day 7 part 2 solution

---
+++ day07_2/src/main.rs
@@ -0,0 +1,43 @@
+use std::fs::read_to_string;
+
+fn main() {
+    let input = parse_input(read_to_string("input.txt").unwrap());
+    let mut total_calibration_result = 0;
+    for (result, numbers) in input {
+        for mut i in 0..3_u32.pow(u32::try_from(numbers.len() - 1).unwrap()) {
+            let mut numbers = numbers.iter();
+            let mut test_result = *numbers.next().unwrap();
+            for n in numbers {
+                if i % 3 == 0 {
+                    test_result += n;
+                } else if i % 3 == 1 {
+                    test_result *= n;
+                } else {
+                    test_result *= 10_u64.pow(n.ilog10() + 1);
+                    test_result += n;
+                }
+                i /= 3;
+            }
+
+            if test_result == result {
+                total_calibration_result += result;
+                break;
+            }
+        }
+    }
+
+    println!("{total_calibration_result}");
+}
+
+fn parse_input(input: String) -> Vec<(u64, Vec<u64>)> {
+    let mut output = Vec::new();
+    for line in input.lines() {
+        let (result, numbers) = line.split_once(": ").unwrap();
+        output.push((
+            result.parse().unwrap(),
+            numbers.split(' ').map(|s| s.parse().unwrap()).collect(),
+        ));
+    }
+
+    output
+}
